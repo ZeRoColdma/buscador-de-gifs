46a98c39a43eb25a257cc990d1446bf1dc27f92d
Zero Cold <ccarloos9@gmail.com>
2018-12-27 07:53:25 -0300

    Correcoes

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:buscado_de_gifs/ui/home_page.dart';
 
-class Home extends StatefulWidget {
-  _HomeState createState() => _HomeState();
+void main() => runApp(MaterialApp(
+      home: MyApp(),
+    ));
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class _HomeState extends State<Home> {
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return 
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ola"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32.0),
+        child: Center(
+          child: Column(
+            children: <Widget>[],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,22 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   String _search;
+  int _offSet = 0;
 
-  _getSearch() async{
+  Future<Map> _getGifs() async {
     http.Response response;
 
-    if (_search == null){
-      response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=&q=dogs&limit=25&offset=75&rating=G&lang=en");
+    if (_search == null) {
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=GeAUobdXzADy3PdBIQCkiWYhUjBdguhl=&limit=25&rating=G");
+    } else {
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=GeAUobdXzADy3PdBIQCkiWYhUjBdguhl&q=$_search&limit=20&offset=$_offSet&rating=G&lang=en");
     }
-
+    return json.decode(response.body);
   }
+
+  @override
+    void initState() {
+      // TODO: implement initState
+      _getGifs().then((map){
+        print(map);
+      });
+    }
 
   @override
   Widget build(BuildContext context) {

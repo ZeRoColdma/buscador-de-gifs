ea217b8f2c124f0e7836ba5fec01a76b0726a3fb
Zero Cold <ccarloos9@gmail.com>
2018-12-28 10:17:26 -0300

    Correços

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:buscado_de_gifs/ui/home_page.dart';
 
+
 void main() => runApp(MaterialApp(
-      home: MyApp(),
+      home: HomePage(),
     ));
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Ola"),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(32.0),
-        child: Center(
-          child: Column(
-            children: <Widget>[],
-          ),
-        ),
-      ),
-    );
-  }
-}
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -15,24 +15,82 @@ class _HomePageState extends State<HomePage> {
 
     if (_search == null) {
       response = await http.get(
-          "https://api.giphy.com/v1/gifs/trending?api_key=GeAUobdXzADy3PdBIQCkiWYhUjBdguhl=&limit=25&rating=G");
+          "https://api.giphy.com/v1/gifs/trending?api_key=GeAUobdXzADy3PdBIQCkiWYhUjBdguhl&limit=25&rating=G");
     } else {
       response = await http.get(
-          "https://api.giphy.com/v1/gifs/search?api_key=GeAUobdXzADy3PdBIQCkiWYhUjBdguhl&q=$_search&limit=20&offset=$_offSet&rating=G&lang=en");
+          "https://api.giphy.com/v1/gifs/search?api_key=GeAUobdXzADy3PdBIQCkiWYhUjBdguhl&q=&limit=25&offset=0&rating=G&lang=en");
     }
     return json.decode(response.body);
   }
 
   @override
-    void initState() {
-      // TODO: implement initState
-      _getGifs().then((map){
-        print(map);
-      });
-    }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+  void initState() {
+    _getGifs().then((map) {
+      print(map);
+    });
   }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        title: Image.network(
+            "https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif"),
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              decoration: InputDecoration(
+                  labelText: "Pesquise aqui",
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder()),
+              style: TextStyle(color: Colors.white, fontSize: 18),
+              textAlign: TextAlign.center,
+              onSubmitted: (text) {
+                setState(() {
+                  _search = text;
+                  _offSet = 0;
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+            future: _getGifs(),
+            builder: (context, snapshot){
+              switch(snapshot.connectionState){
+                case ConnectionState.waiting:
+                case ConnectionState.none:
+                return Container(
+                  width: 200,
+                  height: 200,
+                  alignment: Alignment.center,
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                    strokeWidth: 5,
+                  ),
+                  );
+                  default:
+                  if (snapshot.hasData)return Container(); 
+                }
+              }
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+
+int _getCount(List data){
+  if(_search == null){
+    return data.length;
+  }else{
+    return data.length + 1;
+  }
+}
 }
